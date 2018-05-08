69bb4e61b6fd4d5da999a44b4167ba9f8916f998
Daniel Hook <danhook007@icloud.com>
2018-05-08 10:46:36 -0700

    fixed version number

--- legacy_enhancement_suite.user.js
+++ legacy_enhancement_suite.user.js
@@ -26,7 +26,7 @@
 // @description Improvements to Legacy Game
 // @include     http://www.legacy-game.net/*
 // @include     http://dev.legacy-game.net/*
-// @version     0.0.58
+// @version     0.0.59
 // @grant       none
 // @require     https://raw.githubusercontent.com/nnnick/Chart.js/4aa274d5b2c82e28f7a7b2bb78db23b0429255a1/Chart.js
 // @require     http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.js
